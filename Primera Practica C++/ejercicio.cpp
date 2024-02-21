3fd1b86dcc8619e1f582821bc8770d3cdc4d62c3
edgar6021 <115028351+edgar6021@users.noreply.github.com>
2024-02-21 12:47:43 -0400

    add

---
+++ Primera Practica C++/ejercicio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int args, char argv[])
+{
+   char c='f', d='5';
+   
+
+    cout<< c++ + c++ - --c + ++c;
+
+    cout<< 5 + ++d + ++d + ++d;
+
+    
+}
---
+++ Primera Practica C++/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ( int argc, float argv[]){
+ float k=1.5;
+ float j=3.5;
+ float L=2.5;
+ float m=0.5;
+ float n=5.0;
+
+
+ n += 3*k && 2+ j|| 5-L || 5+L && k-3*m;
+cout <<n <<endl;
+return 0;
+}
